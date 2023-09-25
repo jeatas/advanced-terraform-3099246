cc8cf81639db6cc56e0c3b782afc42d31071472e
jeft <jeatas@gmail.com>
2023-09-25 11:26:52 -0400

    main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "iconic-episode-400112" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
